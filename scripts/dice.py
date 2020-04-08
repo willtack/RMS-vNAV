3fc2fafcf6ef2ab61781cd7b8d8e481e46dcedff
Will Tackett <will.s.tackett@gmail.com>
2020-04-07 20:09:01 -0400

    big update

--- scripts/dice.py
+++ scripts/dice.py
@@ -1,3 +1,7 @@
+#
+# Calculate Dice coefficients for pairs of within session scans
+# and consolidate data into a couple csvs
+
 import nipype.algorithms.metrics as metrics
 import numpy as np
 import pandas as pd
@@ -6,32 +10,39 @@ import os
 labels = [2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 26, 28, 30, 31, 41, 42, 43, 44, 46, 47, 49, 50, 51, 52, 53, 54, 58, 60, 62, 63, 77, 85, 251, 252, 253, 254, 255]
 
 data_dir = os.path.join('/','home','will','Projects','RMS','data')
+# initialize dictionary that will make it easy to visualize avg dice, etc. by subject
 data_dict = {'subject': [], 'mode': [], 'dice': [], 'jaccard': []}
-rois_dict = {'labels': labels}
+rois_dict = {'labels': labels} # for visualizing data by ROI
 roi_df = pd.DataFrame(data=rois_dict)
 
 for subdir, dirs, files in os.walk(data_dir):
+    # get lists of the passive and active segmentation files for this subject
     passive_list = [f for f in files if "passive" in f and 'mni152.nii' in f]
     active_list = [f for f in files if "active" in f and 'mni152.nii' in f]
-    subject = subdir.split('/')[-1]
+    subject = subdir.split('/')[-1] # subject name is the last element in the path
     print(subject)
     for list in passive_list, active_list:
-        if len(list) > 1:
+        if len(list) > 1: # make sure list isn't empty
+            # determine the mode and the two relevant images
             mode = list[0].split("_")[4]
             vol1 = os.path.join(subdir, list[0])
             vol2 = os.path.join(subdir, list[1])
+            # Run Overlap analysis and store outputs
             overlap = metrics.Overlap(volume1=vol1,volume2=vol2,vol_units='mm')
             res=overlap.run()
             dice = res.outputs.dice
             jaccard = res.outputs.jaccard
             roi_di = res.outputs.roi_di
+            # append outputs to first dictionary
             data_dict['subject'].append(subject)
             data_dict['mode'].append(mode)
             data_dict['dice'].append(dice)
             data_dict['jaccard'].append(jaccard)
-            if len(roi_di) == 43:
+            # append the list of DF
+            if len(roi_di) == 43: # should be 43 regions. dims need to be = for concatenation
                 roi_df[subject] = roi_di
 
+# create dataframe from first dict and save both dfs to csvs
 print(data_dict)
 df = pd.DataFrame(data=data_dict)
 csv_file = df.to_csv(os.path.join(data_dir, '..', 'dice2.csv'))
--- scripts/download_files.py
+++ scripts/download_files.py
@@ -1,10 +1,14 @@
 import flywheel
 import os
+import glob
 
-fw = flywheel.Client()
+fw = flywheel.Client() # establish flywheel Client
+# Define paths for impt variables
 project = fw.lookup('huntington/Pilot_Study')
 outdir = os.path.join("/home", "will", "Projects", "RMS", "data")
 freedir = os.path.join("/media", "will", "Data", "freesurfer_subjects")
+
+# retrieve list of relevant analyses
 vv = fw.get_analyses("projects", project.id ,"sessions")
 recon_list = [fw.get(a.id) for a in vv if "recon-all" in a.label]
 
@@ -17,10 +21,18 @@ for a in recon_list:
     sub = fw.get(a.parents.subject)
     subdir = os.path.join(outdir, sub.label)
     os.makedirs(subdir, exist_ok=True)
+    # skip if already downloaded
+    list = glob.glob(os.path.join(subdir,name+'.csv'))
+    print(list)
+    if len(list) > 0:
+        print("Skipping {0}'s {1}".format(sub.label, name))
+        continue
     # download files
-    if len(a.files) > 0:
-        vol_stats_csv = [f for f in a.files if '.zip' in f.name]
+    if len(a.files) > 0: # make sure the analysis has completed successfully
+        # make a list with (hopefully) one entry--the file you want
+        # can fill in any string for whatever file you want
+        vol_stats_csv = [f for f in a.files if 'vol_mm3.csv' in f.name]
         if len(vol_stats_csv) > 0:
             a.download_file(vol_stats_csv[0].name, \
-            os.path.join(subdir, vol_stats_csv[0].name))
-            print("Downloading " + vol_stats_csv[0].name)
+            os.path.join(subdir, name+'.csv'))
+            print("Downloading {0} as {1}".format(vol_stats_csv[0].name, name))
