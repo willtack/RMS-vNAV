1d29488263b0943493999cc466f3e808a02cd44f
Will Tackett <will.s.tackett@gmail.com>
2020-05-18 18:41:47 -0400

    small change

--- permutations/permute_all_data.m
+++ permutations/permute_all_data.m
@@ -9,11 +9,11 @@ for i = 1:length(X)
    % convert each integer to base 6
    integer=X(i,1);
    hexi_int = dec2base(integer,6);
-   hexi_int = sprintf('%010s', hexi_int); % pad to 10 places
    % each digit of hexi_int encodes one of 6 permutations for a subject
-   results = zeros(length(subjects),1);
+  results = zeros(length(subjects),1);
    for j=1:length(subjects)
-       diff_matrix = permute_vols(subjects(j), vols_vector);
+       vols_vector = subjects(i,2);
+       diff_matrix = permute_vols(vols_vector);
        permut_idx = hexi_int(j); % the specified permutation for the current subject
        results(j) = diff_matrix(permut_idx);
    end
